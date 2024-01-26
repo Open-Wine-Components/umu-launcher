c3c016bcdc7d107f239e302712a1cce7c845ddc6
GloriousEggroll <gloriouseggroll@gmail.com>
2024-01-26 11:40:12 -0700

    fix parsing of steamappid inside ulwgl ids, pass '0' as steamappid for games with no steam version (https://github.com/ValveSoftware/steam-runtime/issues/287#issuecomment-831877852)

--- gamelauncher.sh
+++ gamelauncher.sh
@@ -17,10 +17,10 @@ if [[ $WINEPREFIX ]]; then
 fi
 export PROTONPATH="$PROTONPATH"
 export ULWGL_ID="$GAMEID"
-export STEAM_COMPAT_APP_ID="$GAMEID"
+export STEAM_COMPAT_APP_ID="0"
 numcheck='^[0-9]+$'
-if [[ $(cat $ULWGL_ID | cut -d "-" -f 2) =~ $numcheck ]]; then
-  export STEAM_COMPAT_APP_ID=$(cat $ULWGL_ID | cut -d "-" -f 2)
+if [[ $(echo $ULWGL_ID | cut -d "-" -f 2) =~ $numcheck ]]; then
+  export STEAM_COMPAT_APP_ID=$(echo $ULWGL_ID | cut -d "-" -f 2)
 fi
 export SteamAppId="$STEAM_COMPAT_APP_ID"
 export SteamGameId="$STEAM_COMPAT_APP_ID"
