8fdd5d679b0f897bef223b138ea5f42510a6d1a8
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2024-01-29 23:16:16 -0800

    gamelauncher_test: add tests for TOML files

--- gamelauncher_test.py
+++ gamelauncher_test.py
@@ -4,6 +4,8 @@ import os
 import argparse
 from argparse import Namespace
 from unittest.mock import patch
+from pathlib import Path
+from tomllib import TOMLDecodeError
 
 
 class TestGameLauncher(unittest.TestCase):
@@ -37,31 +39,224 @@ class TestGameLauncher(unittest.TestCase):
                 os.environ.pop(key)
 
     def test_parse_args(self):
+    def test_set_env_toml_err(self):
+        """Test set_env_toml for valid TOML
+        A TOMLDecodeError should be raised for invalid values
         """
         Test parse_args with no options
+        env = {
+            "WINEPREFIX": "",
+            "GAMEID": "",
+            "CRASH_REPORT": "/tmp/ULWGL_crashreports",
+            "PROTONPATH": "",
+            "STEAM_COMPAT_APP_ID": "",
+            "STEAM_COMPAT_TOOL_PATHS": "",
+            "STEAM_COMPAT_LIBRARY_PATHS": "",
+            "STEAM_COMPAT_MOUNTS": "",
+            "STEAM_COMPAT_INSTALL_PATH": "",
+            "STEAM_COMPAT_CLIENT_INSTALL_PATH": "",
+            "STEAM_COMPAT_DATA_PATH": "",
+            "STEAM_COMPAT_SHADER_PATH": "",
+            "FONTCONFIG_PATH": "",
+            "EXE": "",
+            "LAUNCHARGS": "",
+            "SteamAppId": "",
+        }
+        test_file = "./tmp.WMYQiPb9A"
+        test_toml = "foo.toml"
+        toml_str = f"""
+        [ulwgl]
+        prefix = [[
+        proton = "{test_file}"
+        game_id = "{test_file}"
+        launch_opts = ["{test_file}", "{test_file}"]
         """
         with self.assertRaises(SystemExit):
+        toml_path = test_file + "/" + test_toml
+        result = None
+
+        Path(toml_path).touch()
+
+        with open(toml_path, "w") as file:
+            file.write(toml_str)
+
+        with patch.object(
+            gamelauncher,
+            "parse_args",
+            return_value=argparse.Namespace(config=toml_path),
+        ):
             result = gamelauncher.parse_args()
             self.assertIsInstance(result, Namespace)
             self.assertIsNone(result.config, "--config is not None")
             self.assertIsNone(result.game, "--game is not None")
+            self.assertIsInstance(
+                result, Namespace, "Expected a Namespace from parse_arg"
+            )
+            with self.assertRaisesRegex(TOMLDecodeError, "Invalid"):
+                gamelauncher.set_env_toml(env, result)
+
+    def test_set_env_toml_nodir(self):
+        """Test set_env_toml if certain key/value are not a dir
 
     def test_parse_args_config(self):
+        An IsDirectoryError should be raised if proton or prefix are not directories
         """
         Test parse_args --config
-        """
+        env = {
+            "WINEPREFIX": "",
+            "GAMEID": "",
+            "CRASH_REPORT": "/tmp/ULWGL_crashreports",
+            "PROTONPATH": "",
+            "STEAM_COMPAT_APP_ID": "",
+            "STEAM_COMPAT_TOOL_PATHS": "",
+            "STEAM_COMPAT_LIBRARY_PATHS": "",
+            "STEAM_COMPAT_MOUNTS": "",
+            "STEAM_COMPAT_INSTALL_PATH": "",
+            "STEAM_COMPAT_CLIENT_INSTALL_PATH": "",
+            "STEAM_COMPAT_DATA_PATH": "",
+            "STEAM_COMPAT_SHADER_PATH": "",
+            "FONTCONFIG_PATH": "",
+            "EXE": "",
+            "LAUNCHARGS": "",
+            "SteamAppId": "",
+        }
         test_file = "./tmp.WMYQiPb9A"
+        test_toml = "foo.toml"
+        toml_str = f"""
+        [ulwgl]
+        prefix = "foo"
+        proton = "foo"
+        game_id = "{test_file}"
+        launch_opts = ["{test_file}", "{test_file}"]
+        """
+        toml_path = test_file + "/" + test_toml
+        result = None
+
+        Path(toml_path).touch()
+
+        with open(toml_path, "w") as file:
+            file.write(toml_str)
+
+        with patch.object(
+            gamelauncher,
+            "parse_args",
+            return_value=argparse.Namespace(config=toml_path),
+        ):
+            result = gamelauncher.parse_args()
+            self.assertIsInstance(
+                result, Namespace, "Expected a Namespace from parse_arg"
+            )
+            self.assertTrue(vars(result).get("config"), "Expected a value for --config")
+            with self.assertRaisesRegex(NotADirectoryError, "prefix"):
+                gamelauncher.set_env_toml(env, result)
+
+    def test_set_env_toml_tables(self):
+        """Test set_env_toml for expected tables
+
+        A KeyError should be raised if the table 'ulwgl' is absent
+        """
+        env = {
+            "WINEPREFIX": "",
+            "GAMEID": "",
+            "CRASH_REPORT": "/tmp/ULWGL_crashreports",
+            "PROTONPATH": "",
+            "STEAM_COMPAT_APP_ID": "",
+            "STEAM_COMPAT_TOOL_PATHS": "",
+            "STEAM_COMPAT_LIBRARY_PATHS": "",
+            "STEAM_COMPAT_MOUNTS": "",
+            "STEAM_COMPAT_INSTALL_PATH": "",
+            "STEAM_COMPAT_CLIENT_INSTALL_PATH": "",
+            "STEAM_COMPAT_DATA_PATH": "",
+            "STEAM_COMPAT_SHADER_PATH": "",
+            "FONTCONFIG_PATH": "",
+            "EXE": "",
+            "LAUNCHARGS": "",
+            "SteamAppId": "",
+        }
+        test_file = "./tmp.WMYQiPb9A"
+        test_toml = "foo.toml"
+        toml_str = f"""
+        [foo]
+        prefix = "{test_file}"
+        proton = "{test_file}"
+        game_id = "{test_file}"
+        launch_opts = ["{test_file}", "{test_file}"]
+        """
+        toml_path = test_file + "/" + test_toml
+        result = None
+
+        Path(toml_path).touch()
+
+        with open(toml_path, "w") as file:
+            file.write(toml_str)
+
         with patch.object(
             gamelauncher,
             "parse_args",
             return_value=argparse.Namespace(config=test_file),
+            return_value=argparse.Namespace(config=toml_path),
         ):
             result = gamelauncher.parse_args()
             self.assertIsInstance(result, Namespace)
+            self.assertIsInstance(
+                result, Namespace, "Expected a Namespace from parse_arg"
+            )
+            self.assertTrue(vars(result).get("config"), "Expected a value for --config")
+            with self.assertRaisesRegex(KeyError, "ulwgl"):
+                gamelauncher.set_env_toml(env, result)
 
     def test_parse_args_game(self):
+    def test_set_env_toml(self):
+        """Test set_env_toml"""
+        env = {
+            "WINEPREFIX": "",
+            "GAMEID": "",
+            "CRASH_REPORT": "/tmp/ULWGL_crashreports",
+            "PROTONPATH": "",
+            "STEAM_COMPAT_APP_ID": "",
+            "STEAM_COMPAT_TOOL_PATHS": "",
+            "STEAM_COMPAT_LIBRARY_PATHS": "",
+            "STEAM_COMPAT_MOUNTS": "",
+            "STEAM_COMPAT_INSTALL_PATH": "",
+            "STEAM_COMPAT_CLIENT_INSTALL_PATH": "",
+            "STEAM_COMPAT_DATA_PATH": "",
+            "STEAM_COMPAT_SHADER_PATH": "",
+            "FONTCONFIG_PATH": "",
+            "EXE": "",
+            "LAUNCHARGS": "",
+            "SteamAppId": "",
+        }
+        test_file = "./tmp.WMYQiPb9A"
+        test_toml = "foo.toml"
+        toml_str = f"""
+        [ulwgl]
+        prefix = "{test_file}"
+        proton = "{test_file}"
+        game_id = "{test_file}"
+        launch_opts = ["{test_file}", "{test_file}"]
         """
         Test parse_args --game
+        toml_path = test_file + "/" + test_toml
+        result = None
+        result_set_env = None
+
+        Path(toml_path).touch()
+
+        with open(toml_path, "w") as file:
+            file.write(toml_str)
+
+        with patch.object(
+            gamelauncher,
+            "parse_args",
+            return_value=argparse.Namespace(config=toml_path),
+        ):
+            result = gamelauncher.parse_args()
+            self.assertIsInstance(
+                result, Namespace, "Expected a Namespace from parse_arg"
+            )
+            self.assertTrue(vars(result).get("config"), "Expected a value for --config")
+            result_set_env = gamelauncher.set_env_toml(env, result)
+            self.assertIsNone(result_set_env, "Expected None after parsing TOML")
         """
         test_file = "./tmp.WMYQiPb9A"
         with patch.object(
