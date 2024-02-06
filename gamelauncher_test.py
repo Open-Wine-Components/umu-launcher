dc4f45133a305350fd34b26d8cecfb81dde35a29
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2024-02-05 22:19:53 -0800

    gamelauncher_test.py: add test for --options

--- gamelauncher_test.py
+++ gamelauncher_test.py
@@ -842,6 +842,23 @@ class TestGameLauncher(unittest.TestCase):
                 "Expected the same value when setting --verb",
             )
 
+    def test_parse_args_options(self):
+        """Test parse_args --options."""
+        with patch.object(
+            gamelauncher,
+            "parse_args",
+            return_value=argparse.Namespace(exe=self.test_exe, options=self.test_opts),
+        ):
+            result = gamelauncher.parse_args()
+            self.assertIsInstance(
+                result, Namespace, "Expected a Namespace from parse_arg"
+            )
+            self.assertEqual(
+                result.options,
+                self.test_opts,
+                "Expected the same value when setting --options",
+            )
+
     def test_parse_args(self):
         """Test parse_args with no options.
 
