549118867cf428a2cfafe27dec41027351ad829a
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2024-02-13 13:21:32 -0800

    Add Github Action for ulwgl_*.py files (#1)
    ulwgl_test.py: update test
    
    .github/workflows: add yml file

--- ulwgl_test.py
+++ ulwgl_test.py
@@ -64,9 +64,14 @@ class TestGameLauncher(unittest.TestCase):
     def test_game_drive_empty(self):
         """Test enable_steam_game_drive.
 
-        Empty WINE prefixes can be created by passing an empty string to --exe
+        Empty WINE prefixes can be created by passing an empty string
+        Example:
+        WINEPREFIX= PROTONPATH= GAMEID= ulwgl-run ""
+
         During this process, we attempt to prepare setting up game drive and set the values for STEAM_RUNTIME_LIBRARY_PATH and STEAM_COMPAT_INSTALL_PATHS
         The resulting value of those variables should be colon delimited string with no leading colons and contain only /usr/lib or /usr/lib32
+
+        Ignores LD_LIBRARY_PATH, relevant to Game Drive, which is sourced in Ubuntu and maybe its derivatives
         """
         args = None
         result_gamedrive = None
@@ -88,6 +93,11 @@ class TestGameLauncher(unittest.TestCase):
             ulwgl_run.set_env(self.env, args)
             # Game drive
             result_gamedrive = ulwgl_plugins.enable_steam_game_drive(self.env)
+
+        # Ubuntu sources this variable and will be added once Game Drive is enabled
+        # Just test the case without it
+        if "LD_LIBRARY_PATH" in os.environ:
+            os.environ.pop("LD_LIBRARY_PATH")
 
         for key, val in self.env.items():
             os.environ[key] = val
