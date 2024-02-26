c48867629733ea5d3bf4cd448fe7ec3453722924
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2024-02-26 14:56:09 -0800

    Support symlink of user to steamuser in prefix
    - For new prefixes, a symbolic link is created of the system user to the steamuser directory or vice versa if one of them exists. In the case for new prefixes and for prefixes without a user symbolic link, the name of the link will be determined by the password database.
    
    - Related to https://github.com/Open-Wine-Components/ULWGL-launcher/pull/21#discussion_r1485021984

--- ulwgl_util.py
+++ ulwgl_util.py
@@ -1,5 +1,8 @@
 from ulwgl_consts import Color, Level
 from typing import Any
+from os import getuid
+from pathlib import Path
+from pwd import struct_passwd, getpwuid
 
 
 def msg(msg: Any, level: Level):
@@ -18,3 +21,33 @@ def msg(msg: Any, level: Level):
         log = f"{Color.BOLD.value}{Color.DEBUG.value}{msg}{Color.RESET.value}"
 
     return log
+
+
+class UnixUser:
+    """Represents the User of the system as determined by the password database rather than environment variables or file system paths."""
+
+    def __init__(self):
+        """Immutable properties of the user determined by the password database that's derived from the real user id."""
+        uid: int = getuid()
+        entry: struct_passwd = getpwuid(uid)
+        # Immutable properties, hence no setters
+        self.name: str = entry.pw_name
+        self.puid: str = entry.pw_uid  # Should be equivalent to the value from getuid
+        self.dir: str = entry.pw_dir
+        self.is_user: bool = self.puid == uid
+
+    def get_home_dir(self) -> Path:
+        """User home directory as determined by the password database that's derived from the current process's real user id."""
+        return Path(self.dir).as_posix()
+
+    def get_user(self) -> str:
+        """User (login name) as determined by the password database that's derived from the current process's real user id."""
+        return self.name
+
+    def get_puid(self) -> int:
+        """Numerical user ID as determined by the password database that's derived from the current process's real user id."""
+        return self.puid
+
+    def is_user(self, uid: int) -> bool:
+        """Compare the UID passed in to this instance."""
+        return uid == self.puid
