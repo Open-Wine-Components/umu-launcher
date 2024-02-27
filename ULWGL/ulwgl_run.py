02e5db4133ec71fb97588b6a8289b24f9fe561d3
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2024-02-27 14:00:53 -0800

    ulwgl_util: Ruff format

--- ULWGL/ulwgl_run.py
+++ ULWGL/ulwgl_run.py
@@ -15,10 +15,12 @@ from ulwgl_util import msg, setup_ulwgl
 from ulwgl_log import log, console_handler, debug_formatter
 from ulwgl_util import UnixUser
 
+
 def is_musl_system() -> bool:
     """Check if the system uses musl libc."""
     # Check for the presence of musl-specific paths in LD_LIBRARY_PATH
-    return 'musl' in os.environ.get('LD_LIBRARY_PATH', '')
+    return "musl" in os.environ.get("LD_LIBRARY_PATH", "")
+
 
 if is_musl_system():
     print("This script is not designed to run on musl-based systems.")
@@ -274,9 +276,7 @@ def build_command(
     if not entry_point:
         home: str = Path.home().as_posix()
         dir: str = Path(__file__).parent.as_posix()
-        msg: str = (
-            f"Path to _v2-entry-point cannot be found in: {home}/.local/share or {dir}\nPlease install a Steam Runtime platform"
-        )
+        msg: str = f"Path to _v2-entry-point cannot be found in: {home}/.local/share or {dir}\nPlease install a Steam Runtime platform"
         raise FileNotFoundError(msg)
 
     if not Path(env.get("PROTONPATH")).joinpath("proton").is_file():
