e303662a60f54a9ebb46ce413bb9a20f5b45093c
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2024-04-03 13:20:29 -0700

    umu_util: remove link

--- umu/umu_util.py
+++ umu/umu_util.py
@@ -1,6 +1,6 @@
 from tarfile import open as tar_open, TarInfo
 from os import environ
-from umu_consts import CONFIG, STEAM_COMPAT, UMU_LOCAL, MODE, FLATPAK_PATH
+from umu_consts import CONFIG, STEAM_COMPAT, UMU_LOCAL, MODE
 from typing import Any, Dict, List, Callable
 from json import load, dump
 from umu_log import log
@@ -188,11 +188,6 @@ def _install_umu(
         root.joinpath("umu-launcher"),
         steam_compat.joinpath("umu-launcher"),
     )
-    # Only create the link for native system packages
-    if not (FLATPAK_PATH or root == Path("/app/share/umu")):
-        steam_compat.joinpath("umu-launcher", "umu-run").symlink_to(
-            "../../../umu/umu_run.py"
-        )
 
     # Runtime platform
     setup_runtime(json)
@@ -338,10 +333,6 @@ def _update_umu(
                     steam_compat.joinpath("umu-launcher"),
                 )
 
-                if not (FLATPAK_PATH or root == Path("/app/share/umu")):
-                    steam_compat.joinpath("umu-launcher", "umu-run").symlink_to(
-                        "../../../umu/umu_run.py"
-                    )
                 log.console(f"Restored umu-launcher to {val}")
             elif steam_compat.joinpath("umu-launcher").is_dir() and val != runner:
                 # Update
@@ -352,11 +343,6 @@ def _update_umu(
                     root.joinpath("umu-launcher"),
                     steam_compat.joinpath("umu-launcher"),
                 )
-
-                if not (FLATPAK_PATH or root == Path("/app/share/umu")):
-                    steam_compat.joinpath("umu-launcher", "umu-run").symlink_to(
-                        "../../../umu/umu_run.py"
-                    )
 
                 json_local["umu"]["versions"]["runner"] = val
 
