08289ad5f63dcb5b0d1a68928b150820470e10e9
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2024-06-04 20:01:12 -0700

    umu_util: add winetricks functionality

--- umu/umu_util.py
+++ umu/umu_util.py
@@ -1,5 +1,7 @@
 from ctypes.util import find_library
 from functools import lru_cache
+from pathlib import Path
+from re import match
 from shutil import which
 from subprocess import PIPE, STDOUT, Popen, TimeoutExpired
 
@@ -61,3 +63,34 @@ def run_zenity(command: str, opts: list[str], msg: str) -> int:
         log.warning("zenity exited with the status code: %s", ret)
 
     return ret
+
+
+def _parse_winetricks_verbs(verb: str, pfx: Path) -> bool:
+    """Parse the winetricks.log file."""
+    wt_log: Path = pfx.joinpath("winetricks.log")
+    is_installed: bool = False
+
+    if not wt_log.is_file():
+        return is_installed
+
+    with wt_log.open(mode="r", encoding="utf-8") as file:
+        for line in file:
+            _ = line.strip()
+            if is_winetricks_verb(_) and _.startswith(verb):
+                is_installed = True
+                break
+
+    return is_installed
+
+
+@lru_cache
+def is_winetricks_verb(
+    verb: str, pattern: str = r"^[a-zA-Z_0-9]+(=[a-zA-Z0-9]+)?$"
+) -> bool:
+    """Check if a string is a winetricks verb."""
+    return match(pattern, verb) is not None
+
+
+def is_installed_verb(verb: str, pfx: Path) -> bool:
+    """Check if a winetricks verb is installed in the umu prefix."""
+    return _parse_winetricks_verbs(verb, pfx)
