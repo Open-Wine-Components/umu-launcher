615d3ab7ff3a4ca9bee41e84c27d5988b516ce18
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2024-10-05 11:23:19 -0700

    __init__: add runtime metadata
    - Metadata will be used to dynamically download and change to the
      correct runtime

--- umu/__init__.py
+++ umu/__init__.py
@@ -1,2 +1,6 @@
 __version__ = "1.1.1"  # noqa: D104
-__runtime_platform__ = "sniper"
+__pressure_vessel_runtime__ = ("steamrt3", "sniper", "1628350")
+__pressure_vessel_runtimes__ = (
+    ("steamrt3", "sniper", "1628350"),
+    ("steamrt2", "soldier", "1391110"),
+)
