a2e0e48f1425d4e9ae496ac6789b7e00da44bec3
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2024-10-09 10:26:51 -0700

    tests: add test for config file usage

---
+++ tests/test_config.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+tmp=$(mktemp)
+mkdir -p "$HOME/.local/share/Steam/compatibilitytools.d"
+mkdir -p "$HOME/Games/umu/umu-0"
+curl -LJO "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton9-15/GE-Proton9-15.tar.gz"
+tar xafv GE-Proton9-15.tar.gz
+mv GE-Proton9-15 "$HOME/.local/share/Steam/compatibilitytools.d"
+
+echo "[umu]
+proton = '~/.local/share/Steam/compatibilitytools.d/GE-Proton9-15'
+game_id = 'umu-0'
+prefix = '~/Games/umu/umu-0'
+exe = '~/.wine/drive_c/windows/syswow64/wineboot.exe'
+launch_args = ['-u']
+" >> "$tmp"
+
+
+cd $HOME && UMU_LOG=debug GAMEID=umu-0 "$HOME/.local/bin/umu-run" --config "$tmp"
