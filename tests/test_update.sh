e8a13d0f1e4d8c26902e89f651ff169ff26f914d
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2024-10-08 18:05:57 -0700

    tests: fix tar extract error

--- tests/test_update.sh
+++ tests/test_update.sh
@@ -3,7 +3,7 @@
 mkdir -p $HOME/.local/share/umu
 
 curl -LJO "https://repo.steampowered.com/steamrt3/images/0.20240916.101795/SteamLinuxRuntime_sniper.tar.xz"
-tar xaf SteamLinuxRuntime_sniper.tar.xz .
+tar xaf SteamLinuxRuntime_sniper.tar.xz
 mv SteamLinuxRuntime_sniper/* $HOME/.local/share/umu
 mv $HOME/.local/share/umu/_v2-entry-point $HOME/.local/share/umu/umu
 
