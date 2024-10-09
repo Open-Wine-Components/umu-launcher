29b3be4286da70f84fd7c283b621c6bd234211bd
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2024-10-09 11:16:38 -0700

    tests: mock wineboot exe

--- tests/test_config.sh
+++ tests/test_config.sh
@@ -4,7 +4,8 @@ python --version
 
 tmp=$(mktemp)
 mkdir -p "$HOME/.local/share/Steam/compatibilitytools.d"
-mkdir -p "$HOME/Games/umu/umu-0"
+mkdir -p "$HOME/Games/umu/umu-0/drive_c/windows/syswow64"
+touch "$HOME/Games/umu/umu-0/drive_c/windows/syswow64/wineboot.exe"
 curl -LJO "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton9-15/GE-Proton9-15.tar.gz"
 tar xaf GE-Proton9-15.tar.gz
 mv GE-Proton9-15 "$HOME/.local/share/Steam/compatibilitytools.d"
@@ -13,7 +14,7 @@ echo "[umu]
 proton = '~/.local/share/Steam/compatibilitytools.d/GE-Proton9-15'
 game_id = 'umu-0'
 prefix = '~/Games/umu/umu-0'
-exe = '~/.wine/drive_c/windows/syswow64/wineboot.exe'
+exe = '~/Games/umu/umu-0/drive_c/windows/syswow64/wineboot.exe'
 launch_args = ['-u']
 " >> "$tmp"
 
