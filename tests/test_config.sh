fe931ab5e84c5a1baf1a8359723a1d099000f82b
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2024-10-25 15:52:37 -0700

    tests: bump umu proton version

--- tests/test_config.sh
+++ tests/test_config.sh
@@ -5,17 +5,18 @@ python --version
 tmp=$(mktemp)
 mkdir -p "$HOME/.local/share/Steam/compatibilitytools.d"
 mkdir -p "$HOME/Games/umu/umu-0"
-curl -LJO "https://github.com/Open-Wine-Components/umu-proton/releases/download/UMU-Proton-9.0-3/UMU-Proton-9.0-3.tar.gz"
-tar xaf UMU-Proton-9.0-3.tar.gz
-mv UMU-Proton-9.0-3 "$HOME/.local/share/Steam/compatibilitytools.d"
+curl -LJO "https://github.com/Open-Wine-Components/umu-proton/releases/download/UMU-Proton-9.0-3/UMU-Proton-9.0-3.2.tar.gz"
+tar xaf UMU-Proton-9.0-3.2.tar.gz
+mv UMU-Proton-9.0-3.2 "$HOME/.local/share/Steam/compatibilitytools.d"
 
 echo "[umu]
-proton = '~/.local/share/Steam/compatibilitytools.d/UMU-Proton-9.0-3'
-game_id = 'umu-0'
+proton = '~/.local/share/Steam/compatibilitytools.d/UMU-Proton-9.0-3.2'
+game_id = 'umu-1141086411'
 prefix = '~/Games/umu/umu-0'
 exe = '~/Games/umu/umu-0/drive_c/windows/syswow64/wineboot.exe'
 launch_args = ['-u']
 " >> "$tmp"
 
 
-UMU_LOG=debug GAMEID=umu-0 "$PWD/.venv/bin/python" "$HOME/.local/bin/umu-run" --config "$tmp"
+# Run the 'game' and ensure the protonfixes module finds its fix in umu-database.csv
+UMU_LOG=debug GAMEID=umu-1141086411 STORE=gog "$PWD/.venv/bin/python" "$HOME/.local/bin/umu-run" --config "$tmp" >> /tmp/umu-log.txt && grep -E "INFO: Non-steam game Silent Hill 4: The Room \(umu-1141086411\)" /tmp/umu-log.txt
