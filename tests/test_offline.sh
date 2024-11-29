44210bd4d921e71c0221a49e46b828df5601808a
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2024-11-29 13:23:03 -0800

    tests: fix e2e tests

--- tests/test_offline.sh
+++ tests/test_offline.sh
@@ -2,6 +2,9 @@
 
 name=$(curl -L "https://api.github.com/repos/Open-Wine-Components/umu-proton/releases/latest" | jq .assets[1].name | tr -d '"')
 
+# Ensure the Proton directory doesn't exist
+rm "$name"
+
 # Download runtime
 curl -LJO "https://repo.steampowered.com/steamrt3/images/latest-container-runtime-public-beta/SteamLinuxRuntime_sniper.tar.xz"
 url=$(curl -L "https://api.github.com/repos/Open-Wine-Components/umu-proton/releases/latest" | jq .assets[1].browser_download_url | tr -d '"')
@@ -12,9 +15,8 @@ curl -LJO "$url"
 mkdir -p "$HOME"/.local/share/compatibilitytools.d "$HOME"/.local/share/umu
 
 # Extract the archives
-tar xaf "$name" -C "$HOME"/.local/share/compatibilitytools.d &
-tar xaf SteamLinuxRuntime_sniper.tar.xz &
-wait
+tar xaf "$name" -C "$HOME"/.local/share/compatibilitytools.d
+tar xaf SteamLinuxRuntime_sniper.tar.xz .
 
 cp -a SteamLinuxRuntime_sniper.tar.xz/* "$HOME"/.local/share/umu
 mv "$HOME"/.local/share/umu/_v2-entry-point "$HOME"/.local/share/umu/umu
