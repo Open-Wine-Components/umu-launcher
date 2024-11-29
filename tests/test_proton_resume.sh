96f08661e5492d2d61863e49c1d7e21df7429471
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2024-11-29 12:20:43 -0800

    tests: add test for proton resumption

---
+++ tests/test_proton_resume.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+tmp=$(mktemp)
+name=$(curl -L "https://api.github.com/repos/Open-Wine-Components/umu-proton/releases/latest" | jq .assets[1].name | tr -d '"')
+url=$(curl -L "https://api.github.com/repos/Open-Wine-Components/umu-proton/releases/latest" | jq .assets[1].browser_download_url | tr -d '"')
+# Request the first 100MB of the latest UMU-Proton release
+curl -LJO --range 0-104857599 "$url"
+# Move the incomplete file to our cache to be picked up
+# Note: Must include the *.parts extension
+mkdir -p $HOME/.cache/umu
+mv "$name" "$HOME"/.cache/umu/"$name".parts
+UMU_LOG=debug GAMEID=umu-0 "$HOME/.local/bin/umu-run" wineboot -u 2> "$tmp"
+grep "resuming" "$tmp"
