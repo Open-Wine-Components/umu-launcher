e3766205a79de16858f6a9c3df2df76089add017
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2024-12-05 12:03:51 -0800

    nix: add truststore as dependency

--- packaging/nix/umu-launcher.nix
+++ packaging/nix/umu-launcher.nix
@@ -20,6 +20,7 @@ python3Packages.buildPythonPackage {
     pkgs.python3Packages.xlib
     pkgs.python3Packages.filelock
     pkgs.python3Packages.urllib3
+    pkgs.python3Packages.truststore
   ];
   makeFlags = [ "PYTHON_INTERPRETER=${pyth1}/bin/python" "SHELL_INTERPRETER=/run/current-system/sw/bin/bash" "DESTDIR=${placeholder "out"}" ];
   dontUseMesonConfigure = true;
