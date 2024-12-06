562e3900eebde0fb276dd2f425c097c8b44ccb92
beh_10257 <77834479+beh-10257@users.noreply.github.com>
2024-12-06 19:32:51 +0100

    expose truststore to users and update README to show users that there is the option to remove it if they want (to make it optional) (#300)
    * nix flake update and expose trustore options to users
    
    * make it so the README informs the users about truststore
    
    ---------
    
    Co-authored-by: beh-10257 <beh-10257@users.noreply.github.com>

--- packaging/nix/combine.nix
+++ packaging/nix/combine.nix
@@ -1,8 +1,8 @@
-{ env, package, symlinkJoin,version }:
+{ env, package, symlinkJoin,version,truststore }:
 symlinkJoin {
   name = "umu-run-bwrap";
   paths = [
-    (package.override {version = "${version}";})
+    (package.override {version = "${version}";truststore = truststore;})
     (env.override {version = "${version}";})
   ];
   postBuild = ''
