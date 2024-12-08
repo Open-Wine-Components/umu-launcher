9d6b8e944abe47eb356fbb9022e9e5ca4a9f9077
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2024-12-08 10:08:07 -0800

    tests: assert log statement of exit status
    - Ensures there were no errors between the resumption and launching the executable within the Steam Linux Runtime.

--- tests/test_resume.sh
+++ tests/test_resume.sh
@@ -10,4 +10,4 @@ mkdir -p "$HOME"/.cache/umu
 # Note: Must include the *.parts extension
 mv SteamLinuxRuntime_sniper.tar.xz "$HOME"/.cache/umu/SteamLinuxRuntime_sniper.tar.xz."$id".parts
 UMU_LOG=debug GAMEID=umu-0 "$HOME/.local/bin/umu-run" wineboot -u 2> "$tmp"
-grep "resuming" "$tmp"
+grep "resuming" "$tmp" && grep "exited with wait status" "$tmp"
