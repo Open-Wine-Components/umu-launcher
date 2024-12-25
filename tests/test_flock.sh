5c722020a3e714a59316b08b7c0f9d2bbf865350
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2024-12-24 16:41:56 -0800

    tests: update file lock test

--- tests/test_flock.sh
+++ tests/test_flock.sh
@@ -13,16 +13,15 @@ sleep 1
 UMU_LOG=debug GAMEID=umu-0 "$HOME/.local/bin/umu-run" wineboot -u 2> "$tmp2" &
 wait
 
-grep "exited with wait status" "$tmp1"
+grep "exited with wait status" "$tmp1" && grep -E "exited with wait status" "$tmp2"
 
 # Ensure the 2nd proc didn't download the runtime
-if ! grep -E "\(latest\), please wait..." "$tmp2"; then
+if grep -E "\(latest\), please wait..." "$tmp2"; then
 	exit 1
 fi
 
 # Ensure the 2nd proc didn't download Proton
-if ! grep "Downloading" "$tmp2"; then
+if grep "Downloading" "$tmp2"; then
 	exit 1
 fi
 
-grep -E "exited with wait status" "$tmp2"
