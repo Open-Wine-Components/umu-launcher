78bcb5b767ba17a4a0e524305036baa0392fcdb0
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2024-12-24 16:34:45 -0800

    tests: update file lock test

--- tests/test_flock.sh
+++ tests/test_flock.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
-set -e
 
 #
 # Ensure umu-launcher does not download its fetched resources more than once
@@ -17,9 +16,13 @@ wait
 grep "exited with wait status" "$tmp1"
 
 # Ensure the 2nd proc didn't download the runtime
-! grep -E "\(latest\), please wait..." "$tmp2"
+if ! grep -E "\(latest\), please wait..." "$tmp2"; then
+	exit 1
+fi
 
 # Ensure the 2nd proc didn't download Proton
-! grep "Downloading" "$tmp2" 
+if ! grep "Downloading" "$tmp2"; then
+	exit 1
+fi
 
 grep -E "exited with wait status" "$tmp2"
