0629cc4d3a32571260c0d581cc8e0738e4ba26ae
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2024-12-28 13:12:50 -0800

    umu_bspatch: refactor to handle new patch data and format

--- umu/umu_bspatch.py
+++ umu/umu_bspatch.py
@@ -2,6 +2,7 @@ import os
 from concurrent.futures import Future, ThreadPoolExecutor
 from contextlib import suppress
 from enum import Enum
+from mmap import ACCESS_READ, ACCESS_WRITE, MADV_DONTNEED, mmap
 from pathlib import Path
 from shutil import rmtree
 from typing import TypedDict
@@ -10,13 +11,8 @@ from umu.umu_log import log
 from umu.umu_util import memfdfile
 
 with suppress(ModuleNotFoundError):
-    from .umu_delta import (
-        bspatch_rs,
-        bz2_decompress_rs,
-        crc32_mmap_rs,
-        crc32_rs,
-        zstd_decompress_rs,
-    )
+    from pyzstd import DParameter, ZstdDict, decompress
+    from xxhash import xxh3_64_intdigest
 
 
 class FileType(Enum):  # noqa: D101
@@ -32,7 +28,7 @@ class FileType(Enum):  # noqa: D101
 
 
 class Entry(TypedDict):  # noqa: D101
-    # BSDIFF data, bz2 compressed data or symbolic link's target
+    # BSDIFF data, zstd compressed data or symbolic link's target
     data: bytes
     # File mode bits as decimal
     mode: int
@@ -41,8 +37,8 @@ class Entry(TypedDict):  # noqa: D101
     name: str
     # File's type
     type: FileType
-    # CRC32 result after applying the binary patch
-    cksum: int
+    # xxhash result after applying the binary patch
+    xxhash: int
     # File's modification time
     time: float
     # File's size
@@ -55,8 +51,8 @@ class ManifestEntry(TypedDict):  # noqa: D101
     # File's name as a relative path with the base name of the ommitted
     # e.g., protonfixes/gamefixes-umu/umu-zenlesszonezero.py
     name: str
-    # CRC32 result
-    cksum: int
+    # xxhash result
+    xxhash: int
     # File size
     size: int
     # File modification time
@@ -85,6 +81,8 @@ class ContentContainer(TypedDict):  # noqa: D101
 
 MMAP_MIN = 16 * 1024
 
+ZSTD_WINDOW_LOG_MIN = 10
+
 
 class CustomPatcher:  # noqa: D101
     def __init__(  # noqa: D107
@@ -108,7 +106,7 @@ class CustomPatcher:  # noqa: D101
         for item in self._arc_contents["add"]:
             build_file: Path = self._proton.joinpath(item["name"])
             if item["type"] == FileType.File.value:
-                # Decompress the bz2 data and write the file
+                # Decompress the zstd data and write the file
                 self._futures.append(
                     self._thread_pool.submit(
                         self._write_proton_file, build_file, item
@@ -227,19 +225,25 @@ class CustomPatcher:  # noqa: D101
         try:
             with rpath.open("rb") as fp:
                 stats: os.stat_result = os.fstat(fp.fileno())
-                cksum: int = 0
+                xxhash: int = 0
+
                 if item["size"] != stats.st_size:
                     return None
+
                 if item["mode"] != stats.st_mode:
                     return None
+
                 if item["time"] != stats.st_mtime:
                     return None
-                # Following blake3's heuristic, don't use mmap if < 16KB
+
                 if stats.st_size > MMAP_MIN:
-                    cksum = crc32_mmap_rs(fp.fileno())
+                    with mmap(fp.fileno(), length=0, access=ACCESS_READ) as mm:
+                        xxhash = xxh3_64_intdigest(mm)
+                        mm.madvise(MADV_DONTNEED, 0, stats.st_size)
                 else:
-                    cksum = crc32_rs(fp.fileno())
-                if item["cksum"] != cksum:
+                    xxhash = xxh3_64_intdigest(fp.read())
+
+                if item["xxhash"] != xxhash:
                     return None
         except FileNotFoundError:
             log.debug("Aborting partial update, file not found: %s", rpath)
@@ -249,89 +253,104 @@ class CustomPatcher:  # noqa: D101
 
     def _patch_proton_file(self, path: Path, item: Entry) -> None:
         bdiff: bytes = item["data"]
-        digest: int = item["cksum"]
+        digest: int = item["xxhash"]
         mode: int = item["mode"]
         time: float = item["time"]
+        size: int = item["size"]
 
         try:
             # Since some wine binaries are missing the writable bit and
             # we're memory mapping files, before applying a binary patch,
             # ensure the file is writable
             os.chmod(path, 0o700, follow_symlinks=False)  # noqa: PTH101
+
             # With our patch file, apply the delta in place
             with path.open("rb+") as fp:
-                stats: os.stat_result
-                cksum: int = 0
+                stats: os.stat_result = os.stat(fp.fileno())  # noqa: PTH116
+                xxhash: int = 0
 
-                # Apply the patch
-                bspatch_rs(fp.fileno(), bdiff)
+                if stats.st_size < size:
+                    fp.truncate(size)
 
-                # Compute our expected digest
-                # Following blake3's heuristic, don't use mmap if < 16KB
-                stats = os.fstat(fp.fileno())
-                if stats.st_size > MMAP_MIN:
-                    cksum = crc32_mmap_rs(fp.fileno())
-                else:
-                    cksum = crc32_rs(fp.fileno())
+                if max(stats.st_size, size).bit_length() < ZSTD_WINDOW_LOG_MIN:
+                    fp.write(bdiff)
+                    fp.truncate(size)
+                    os.lseek(fp.fileno(), 0, os.SEEK_SET)
 
-                # Verify our data
-                if cksum != digest:
-                    log.error(
-                        "Expected cksum %s, received %s for file '%s'",
-                        digest,
-                        cksum,
-                        path,
+                    # Verify our data
+                    xxhash = xxh3_64_intdigest(fp.read())
+                    if xxhash != digest:
+                        err: str = f"Expected xxhash {digest}, received {xxhash} for file '{path}' truncating from size {stats.st_size} -> {size}"
+                        raise ValueError(err)
+
+                    os.fchmod(fp.fileno(), mode)
+                    os.utime(fp.fileno(), (stats.st_atime, time))
+                    return
+
+                # Apply our patch to the file in-place
+                with mmap(fp.fileno(), length=0, access=ACCESS_WRITE) as mm:
+                    # Patch the region
+                    zst_dict = ZstdDict(mm[: stats.st_size], is_raw=True)
+                    zst_opt = {DParameter.windowLogMax: 31}
+                    mm[:size] = decompress(
+                        bdiff, zstd_dict=zst_dict.as_prefix, option=zst_opt
                     )
-                    err: str = "Digest mismatch for binary patched result"
-                    raise ValueError(err)
+
+                    # If file will become small, resize our map
+                    if size < stats.st_size:
+                        mm.resize(size)
+
+                    # Compute our expected digest
+                    xxhash = xxh3_64_intdigest(mm)
+
+                    # Verify our data
+                    if xxhash != digest:
+                        err: str = f"Expected xxhash {digest}, received {xxhash} for file '{path}' truncating from size {stats.st_size} -> {size}"
+                        raise ValueError(err)
+
+                    mm.madvise(MADV_DONTNEED, 0, len(mm))
 
                 # Update the file's metadata
                 os.fchmod(fp.fileno(), mode)
-                os.utime(
-                    fp.fileno(),
-                    (stats.st_atime, time),
-                )
-        except (ValueError, KeyboardInterrupt, OSError):
+                os.utime(fp.fileno(), (stats.st_atime, time))
+        except ValueError:
+            raise
+        except (KeyboardInterrupt, OSError) as e:
+            log.exception(e)
             log.error("Binary patch errored for file '%s'", path)
-            log.warning("File '%s' has mode bits 0o700", path)
+            log.warning(
+                "File '%s' may be corrupt and has mode bits 0o700", path
+            )
             raise
 
     def _write_proton_file(self, path: Path, item: Entry) -> None:
         data: bytes = item["data"]
-        digest: int = item["cksum"]
+        digest: int = item["xxhash"]
         mode: int = item["mode"]
         time: float = item["time"]
         size: int = item["size"]
 
         with memfdfile(path.name) as fp:
-            stats: os.stat_result
-            cksum: int = 0
+            xxhash: int = 0
 
-            # Decompress our data and write to our file.
-            zstd_decompress_rs(data, fp.fileno(), size)
+            fp.truncate(size)
 
-            # Following blake3's heuristic, don't use mmap if < 16KB
-            stats = os.fstat(fp.fileno())
-            if stats.st_size > MMAP_MIN:
-                os.lseek(fp.fileno(), 0, os.SEEK_SET)
-                cksum = crc32_mmap_rs(fp.fileno())
-            else:
-                os.lseek(fp.fileno(), 0, os.SEEK_SET)
-                cksum = crc32_rs(fp.fileno())
+            with mmap(fp.fileno(), length=0, access=ACCESS_WRITE) as mm:
+                # Decompress our data and write to our file.
+                mm[:] = decompress(data)
+                xxhash = xxh3_64_intdigest(mm)
 
-            if cksum != digest:
-                log.error(
-                    "Expected %s, received %s for fd '%s' from source '%s'",
-                    digest,
-                    cksum,
-                    fp.fileno(),
-                    path,
-                )
-                err: str = "Digest mismatch when creating file"
-                raise ValueError(err)
+                if xxhash != digest:
+                    err: str = f"Expected xxhash {digest}, received {xxhash} for fd {fp.fileno()} from source {path}"
+                    raise ValueError(err)
 
-            # Write to our file
-            with path.open("wb") as path_fp:
-                os.sendfile(path_fp.fileno(), fp.fileno(), 0, stats.st_size)
-                os.fchmod(path_fp.fileno(), mode)
-                os.utime(path_fp.fileno(), (stats.st_atime, time))
+                os.lseek(fp.fileno(), 0, os.SEEK_CUR)
+
+                # Write to our file
+                stats: os.stat_result = os.fstat(fp.fileno())
+                with path.open("wb") as file:
+                    os.sendfile(file.fileno(), fp.fileno(), 0, size)
+                    os.fchmod(file.fileno(), mode)
+                    os.utime(file.fileno(), (stats.st_atime, time))
+
+                mm.madvise(MADV_DONTNEED, 0, size)
