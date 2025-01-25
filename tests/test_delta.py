52c6f9639865576606c8262e14fc7fb03f15d65b
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2025-01-25 11:08:12 -0800

    test_delta: match patch target and proton

--- tests/test_delta.py
+++ tests/test_delta.py
@@ -23,10 +23,12 @@ def main():  # noqa: D103
         "User-Agent": "",
     }
     codename = "UMU-Latest"
+    ctx = ssl.create_default_context()
 
+    durl = ""
     with urlopen(  # noqa: S310
         Request(url, headers=headers),  # noqa: S310
-        context=ssl.create_default_context(),
+        context=ctx,
     ) as resp:
         releases = json.loads(resp.read())
         for release in releases["assets"]:
@@ -36,12 +38,38 @@ def main():  # noqa: D103
                 durl = release["browser_download_url"]
                 break
 
+    if not durl:
+        print(f"Could not find release with codename '{codename}', skipping")
+        return 0
+
     with urlopen(durl) as resp:  # noqa: S310
         patch = cbor2.loads(resp.read())
         version = patch.get("contents")[0].get("source")
+        target = patch.get("contents")[0].get("target")
 
-    url = f"https://github.com/Open-Wine-Components/umu-proton/releases/download/{version}/{version}.tar.gz"
-    with urlopen(url) as resp:  # noqa: S310
+    # Verify the latest release matches the patch's target
+    url = "https://api.github.com/repos/Open-Wine-Components/umu-proton/releases/latest"
+    durl = ""
+    is_target = False
+    with urlopen(  # noqa: S310
+        Request(url, headers=headers),  # noqa: S310
+        context=ctx,
+    ) as resp:
+        releases = json.loads(resp.read())
+        for release in releases["assets"]:
+            if release["name"].startswith(target) and release["name"].endswith(
+                ".tar.gz"
+            ):
+                is_target = True
+                break
+
+    # Case when the latest release doesn't match the patch target
+    if not is_target:
+        print(f"Latest release is not expected patch target '{target}', skipping")
+        return 0
+
+    durl = f"https://github.com/Open-Wine-Components/umu-proton/releases/download/{version}/{version}.tar.gz"
+    with urlopen(durl) as resp:  # noqa: S310
         buffer = bytearray(64 * 1024)
         view = memoryview(buffer)
         with tempfile.NamedTemporaryFile(mode="ab+", buffering=0) as file:
@@ -60,8 +88,10 @@ def main():  # noqa: D103
         shutil.move(cache.joinpath(version), compat)
         compat.joinpath(version).rename(compat.joinpath("UMU-Latest"))
 
-    exe = shutil.which("umu-run", path="/usr/local/bin")
+    path = "/usr/local/bin"
+    exe = shutil.which("umu-run", path=path)
     if exe is None:
+        print(f"Could not find umu-run in '{path}', exiting")
         return 1
 
     return subprocess.run(
