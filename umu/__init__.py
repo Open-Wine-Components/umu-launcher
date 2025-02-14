317075fe1344be6281168ec9e9246a2211685d96
Glorious Eggroll <gloriouseggroll@gmail.com>
2025-02-14 08:48:23 -0700

    bump version (again)

--- umu/__init__.py
+++ umu/__init__.py
@@ -1,3 +1,3 @@
-__version__ = "1.2.1"  # noqa: D104
+__version__ = "1.2.2"  # noqa: D104
 __runtime_versions__ = (("sniper", "steamrt3"), ("soldier", "steamrt2"))
 __runtime_version__ = __runtime_versions__[0]
