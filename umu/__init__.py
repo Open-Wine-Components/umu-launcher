902c88e7c4ee91409603ca27d353e65831dc5fef
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2025-02-23 15:08:55 -0800

    __init__: update runtime metadata

--- umu/__init__.py
+++ umu/__init__.py
@@ -1,3 +1,6 @@
 __version__ = "1.2.5"  # noqa: D104
-__runtime_versions__ = (("sniper", "steamrt3"), ("soldier", "steamrt2"))
+__runtime_versions__ = (
+    ("sniper", "steamrt3", "1628350"),
+    ("soldier", "steamrt2", "1391110"),
+)
 __runtime_version__ = __runtime_versions__[0]
