92ecc4e2440212ce25b2a71ea6d42574feb35eec
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2025-02-23 16:15:13 -0800

    tests: fix unused tmp

--- tests/test_install_obsolete.sh
+++ tests/test_install_obsolete.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
 
-tmp=$(mktemp)
 mkdir -p "$HOME/.local/share/Steam/compatibilitytools.d"
 curl -LJO "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton7-55/GE-Proton7-55.tar.gz"
 tar xaf GE-Proton7-55.tar.gz
