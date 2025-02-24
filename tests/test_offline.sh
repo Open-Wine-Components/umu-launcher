4759db4ee788c55d48a8813f4dd37cc067c1d614
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2025-02-23 16:38:58 -0800

    tests: set RUNTIMEPATH explicitly when using steamrt3-based protons

--- tests/test_offline.sh
+++ tests/test_offline.sh
@@ -13,20 +13,20 @@ url=$(curl -L "https://api.github.com/repos/Open-Wine-Components/umu-proton/rele
 # Download Proton
 curl -LJO "$url"
 
-mkdir -p "$HOME"/.local/share/Steam/compatibilitytools.d "$HOME"/.local/share/umu "$HOME"/Games/umu
+mkdir -p "$HOME"/.local/share/Steam/compatibilitytools.d "$HOME"/.local/share/umu/steamrt3 "$HOME"/Games/umu
 
 # Extract the archives
 tar xaf "$name" -C "$HOME"/.local/share/Steam/compatibilitytools.d
 tar xaf SteamLinuxRuntime_sniper.tar.xz
 
-cp -a SteamLinuxRuntime_sniper/* "$HOME"/.local/share/umu
-mv "$HOME"/.local/share/umu/_v2-entry-point "$HOME"/.local/share/umu/umu
+cp -a SteamLinuxRuntime_sniper/* "$HOME"/.local/share/umu/steamrt3
+mv "$HOME"/.local/share/umu/steamrt3/_v2-entry-point "$HOME"/.local/share/umu/steamrt3/umu
 
 # Run offline using bwrap
 # TODO: Figure out why the command exits with a 127 when offline. The point
 # is that we're able to enter the container and we do not crash. For now,
 # just query a string that shows that session was offline
-UMU_LOG=debug GAMEID=umu-0 bwrap --unshare-net --bind / / --dev /dev --bind "$HOME" "$HOME" -- "$HOME/.local/bin/umu-run" wineboot -u 2> "$tmp"
+RUNTIMEPATH=steamrt3 UMU_LOG=debug GAMEID=umu-0 bwrap --unshare-net --bind / / --dev /dev --bind "$HOME" "$HOME" -- "$HOME/.local/bin/umu-run" wineboot -u 2> "$tmp"
 
 # Check if we exited. If we logged this statement then there were no errors
 # before entering the container
