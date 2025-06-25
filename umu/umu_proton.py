0bfde105fa63d248561cd9ab42102282d8513a19
Stelios Tsampas <loathingkernel@gmail.com>
2025-06-25 17:52:12 +0300

    chore: reword a some comments

--- umu/umu_proton.py
+++ umu/umu_proton.py
@@ -275,7 +275,7 @@ def _fetch_proton(
     _, http_pool = session_pools
     proton_hash, proton_hash_url = assets[0]
     tarball, tar_url = assets[1]
-    # remove any combination of .xyz.xy suffix (realistically .tar.gz|xz)
+    # remove any combination of .abc.xy suffix (realistically .tar.gz|xz)
     proton = ".".join(tarball.split(".")[:-2])
     ret: int = 0  # Exit code from zenity
     digest: str = ""  # Digest of the Proton archive
@@ -450,7 +450,7 @@ def _get_latest(
         return None
 
     tarball = assets[1][0]
-    # remove any combination of .xyz.xy suffix (realistically .tar.gz|xz)
+    # remove any combination of .abc.xy suffix (realistically .tar.gz|xz)
     proton = ".".join(tarball.split(".")[:-2])
     latest_candidates = {
         ProtonVersion.GELatest.value,
@@ -544,7 +544,7 @@ def _install_proton(
 
     # Move decompressed archive to compatibilitytools.d or
     # $XDG_DATA_HOME/umu/compatibilitytools
-    # remove any combination of .xyz.xy suffix (realistically .tar.gz|xz)
+    # remove any combination of .abc.xy suffix (realistically .tar.gz|xz)
     name = ".".join(tarball.split(".")[:-2])
     folder = cache.joinpath(name)
     if os.environ.get("PROTONPATH") in latest_candidates:
