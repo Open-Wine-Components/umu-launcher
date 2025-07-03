526f5fbc680fe56d1bf3ef97573bf3a7b25417d8
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2025-07-03 01:01:39 +0000

    fix: skip unlinking UMU-Latest in compatibilitytools.d (#504)
    - For users who tinker with their Protons in this directory and an IsADirectoryError can be raised if UMU-Latest is not a symbolic link. Unlinking this file is no longer necessary in 1.2.0+. Related to https://github.com/Open-Wine-Components/umu-launcher/issues/435

--- umu/umu_run.py
+++ umu/umu_run.py
@@ -127,11 +127,8 @@ def check_env(
     if os.environ.get("UMU_NO_PROTON") == "1":
         return env
 
-    path: Path = STEAM_COMPAT.joinpath(os.environ.get("PROTONPATH", ""))
-    if os.environ.get("PROTONPATH") and path.name == "UMU-Latest":
-        path.unlink(missing_ok=True)
-
     # Proton Version
+    path: Path = STEAM_COMPAT.joinpath(os.environ.get("PROTONPATH", ""))
     if os.environ.get("PROTONPATH") and path.is_dir():
         os.environ["PROTONPATH"] = str(STEAM_COMPAT.joinpath(os.environ["PROTONPATH"]))
 
